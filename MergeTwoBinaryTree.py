27ad7bd6e82b69b6c81207099779e4d5c4ad0d33
gaoxy914 <gaoxy@hit.edu,cn>
2018-09-14 11:06:57 +0800

    MergeTwoBinaryTree.py

---
+++ MergeTwoBinaryTree.py
@@ -0,0 +1,15 @@
+class TreeNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution(object):
+	def mergeTrees(self, t1, t2):
+		if t1 != None and t2 != None:
+			t1.val += t2.val
+			t1.left = self.mergeTrees(t1.left, t2.left)
+			t1.right = self.mergeTrees(t1.right, t2.right)
+		if t1 == None and t2 != None:
+			t1 = t2
+		return t1 
