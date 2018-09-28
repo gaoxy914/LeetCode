454e391a520955f3a0199ea4f39eee1ae4137005
gaoxy914 <gaoxy@hit.edu,cn>
2018-09-28 20:40:56 +0800

    BinaryGap.py

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,9 +1,8 @@
 class Solution(object):
 	def binaryGap(self, N):
-		b = "{0:b}",format(N)
+		b = "{0:b}".format(N)
 		dis = [0]
 		last = -1
-		print(b)
 		for i in range(len(b)):
 			if b[i] == '1':
 				print(last)
